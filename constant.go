c1226b69a3372dd4ea86752c5a4f40e14aa364e5
Kody Eckheart <fozzbear123@gmail.com>
2026-07-11 10:21:40 -0700

    fmt remaining files

--- constant.go
+++ constant.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-    "fmt"
-    "math"
+	"fmt"
+	"math"
 )
 
 const s string = "constant"
 
 func main() {
-    fmt.Println("constant:", s)
-    
-    // Has no type until given one, like explicit conversion or by context
-    const n = 500_000_000
-    const f = 50.0 / n
-    fmt.Printf("n type: %T\n", n)
+	fmt.Println("constant:", s)
 
-    const d = 3e20 / n
-    fmt.Println(d)
-    fmt.Println(int64(d))
+	// Has no type until given one, like explicit conversion or by context
+	const n = 500_000_000
+	const f = 50.0 / n
+	fmt.Printf("n type: %T\n", n)
 
-    fmt.Println(math.Sin(n))
-    // If math.Sin expects float, why is n still an int?
-    fmt.Printf("n type: %T\n", n)
+	const d = 3e20 / n
+	fmt.Println(d)
+	fmt.Println(int64(d))
+
+	fmt.Println(math.Sin(n))
+	// If math.Sin expects float, why is n still an int?
+	fmt.Printf("n type: %T\n", n)
 }
